685fb9a9def71b78e0b06275d2daaf995253ef89
Ivoyantpvt <arpithas1920@gmail.com>
2025-12-25 23:57:16 +0530

    EmployeeManagement is modified as salary datatype is changed double to BigDecimal

--- main/java/com/spring/employeeManagement/EmployeeManagementApplication.java
+++ main/java/com/spring/employeeManagement/EmployeeManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
+	//mainclass
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
